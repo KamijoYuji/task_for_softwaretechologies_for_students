a09337b5da86c9e74a6a7c3829ca3d8b01769252
aasorokina4 <annasorsor@mail.ru>
2025-09-26 09:58:05 +0300

    сделаны reverse и rotateMatrix

--- src/main/java/org/softwaretechnologies/ArrayFunctions.java
+++ src/main/java/org/softwaretechnologies/ArrayFunctions.java
@@ -11,6 +11,12 @@ public final class ArrayFunctions {
      */
     public static void reverse(int[] array) {
         // TODO: реализуйте вышеуказанную функцию
+        int length = array.length;
+        for(int i = 0; i < length/2; i++){
+            int temp = array[i];
+            array[i] = array[length-i-1];
+            array[length-i-1] = temp;
+        }
     }
 
     /**
@@ -25,5 +31,29 @@ public final class ArrayFunctions {
      */
     public static void rotateMatrix(int[][] matrix) {
         // TODO: реализуйте вышеуказанную функцию
+        if(matrix.length == 0)
+            return;
+
+        int x = matrix.length;
+        int y = matrix[0].length;
+
+        if(x!=y)
+        {
+            System.out.println("Матрица не квадратная");
+            return;
+        }
+
+        int[][] a = new int[x][y];
+        for(int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                a[i][j] = matrix[j][i];
+                System.out.println(a[i][j]);
+            }
+        }
+
+        for(int i = 0; i < x; i++)
+            for(int j = 0; j < y; j++) {
+                matrix[i][j] = a[i][j];
+            }
     }
 }
