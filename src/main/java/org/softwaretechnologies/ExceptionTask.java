9464b7abc43da2c65fefba9a4e3d0b1440644bcf
aasorokina4 <annasorsor@mail.ru>
2025-10-10 09:54:22 +0300

    task 5

--- src/main/java/org/softwaretechnologies/ExceptionTask.java
+++ src/main/java/org/softwaretechnologies/ExceptionTask.java
@@ -1,5 +1,6 @@
 package org.softwaretechnologies;;
 
+import javax.tools.OptionChecker;
 import java.util.Optional;
 
 public class ExceptionTask {
@@ -9,9 +10,11 @@ public class ExceptionTask {
        Вызвана функция printMessage
      */
     public static void printMessage() {
-        throwRuntimeException();
-        // TODO: реализуйте вышеуказанную функцию
-
+        try {
+            throwRuntimeException();
+        } catch (Exception e){
+            System.out.println("Вызвана функция printMessage");
+        }
     }
 
     /**
@@ -20,8 +23,11 @@ public class ExceptionTask {
      Вызвана функция printMessage2
      */
     public static void printMessage2() throws Exception {
-        throwCatchableException();
-        // TODO: реализуйте вышеуказанную функцию
+        try {
+            throwCatchableException();
+        } catch (Exception e){
+            System.out.println("Вызвана функция printMessage2");
+        }
     }
 
     private static void throwCatchableException() throws Exception {
@@ -40,8 +46,8 @@ public class ExceptionTask {
      * @throws DivideOnNullException если divisor равен 0
      */
     public static int divide(int dividend, int divisor) throws DivideOnNullException {
-
-        // TODO: реализуйте вышеуказанную функцию
+        if(divisor == 0)
+            throw new DivideOnNullException();
         return dividend/divisor;
     }
 
@@ -56,8 +62,14 @@ public class ExceptionTask {
      */
     public static Optional<String> mergeStrings(String first, String second) {
         // TODO: реализуйте вышеуказанную функцию
+        if(first == null&&second == null)
+            return Optional.empty();
 
+        if(first == null)
+            return Optional.of(second);
 
+        if(second == null)
+            return Optional.of(first);
 
         return Optional.of(first.length() > second.length() ? first + second : second + first);
     }
